df63179af2579177451cf2f8554771532bc31103
merouane bel <merouanebelmoubarik2@gmail.com>
2024-10-02 19:53:11 +0100

    difficulty-easy

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int x=121;
-        int x2=-121;
+        System.out.println(solution.romanToInt("LVIII"));
 
-        System.out.println("is the number: "+x+" ? "+solution.isPalindrome(x));
-        System.out.println("is the number: "+x2+" ? "+solution.isPalindrome(x2));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,54 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 class Solution {
-    public boolean isPalindrome(int x) {
-        if(x<0) return false;
-        Integer x1= x;
-        String y = x1.toString();
-        StringBuilder sb = new StringBuilder("");
-        for(int i=y.length()-1;i>=0;i--){
-            sb.append(y.charAt(i));
+    public int romanToInt(String s) {
+        HashMap<String, Integer> numbers = new HashMap<String, Integer>();
+        numbers.put("I", 1);
+        numbers.put("V", 5);
+        numbers.put("X", 10);
+        numbers.put("L", 50);
+        numbers.put("C", 100);
+        numbers.put("D", 500);
+        numbers.put("M", 1000);
+
+        String [] split = s.split("");
+        int result = 0;
+        String a= "";
+
+        for(int i = 0; i < split.length; i++) {
+            if(i+1 <= split.length-1){
+                a = split[i] + split[i+1];
+                switch(a) {
+                    case "IV" : result+=4; i++; break;
+                    case "IX" : result+=9; i++; break;
+                    case "XL" : result+=40; i++; break;
+                    case "XC" : result+=90; i++; break;
+                    case "CD" : result+=400; i++; break;
+                    case "CM" : result+=900; i++; break;
+                    default :
+                        a=split[i];
+                        for(String key : numbers.keySet()) {
+                            if(key.equals(a)){
+                                result+= numbers.get(key) ;
+                            }
+                        }
+                }
+
+            }
+            else {
+                a=split[i];
+                for(String key : numbers.keySet()) {
+                    if(key.equals(a)) {
+                        result +=numbers.get(a);
+                    }
+                }
+            }
         }
-        if (y.equals(sb.toString())){//sb is an object .toString() to covert it to string
-            return true;
-        }
-        else {
-            return false;
-        }
+        return result ;
+
 
 
     }
 }
+
